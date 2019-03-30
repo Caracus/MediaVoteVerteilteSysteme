5c08139ecbbed79e08c39e52b95ed7d07844abf3
Sascha Hans <sascha.hans@web.de>
2019-03-30 22:04:59 +0100

    Added EpisodeBean.java

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/mediavote/episode/ejb/EpisodeBean.java
@@ -0,0 +1,31 @@
+package dhbwka.wwi.vertsys.javaee.mediavote.episode.ejb;
+
+import dhbwka.wwi.vertsys.javaee.mediavote.common.ejb.EntityBean;
+import dhbwka.wwi.vertsys.javaee.mediavote.common.jpa.User;
+import dhbwka.wwi.vertsys.javaee.mediavote.episode.jpa.Episode;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+
+@Stateless
+@RolesAllowed("app-user")
+public class EpisodeBean extends EntityBean<Episode, Long>{
+
+     
+       public EpisodeBean() {
+        super(Episode.class);
+    }
+  
+    public EpisodeBean(Class entityClass) {
+        super(entityClass);
+    }
+    
+    public void createNewEntry(User user,String series, String season, String name, String description){
+        Episode episode = new Episode(user,series,season,name,description);
+        em.persist(episode);
+    }
+    
+    public void updateEntry(User user,String series, String season, String name, String description){
+        Episode episode = new Episode(user,series,season,name,description);
+        em.merge(episode);
+    }
+}
