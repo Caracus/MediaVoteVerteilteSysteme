c20e62d731d8e1062a052adb380be25159150843
Johannes Hoppstädter <webmaster@hoppstaedter.net>
2019-04-01 10:43:06 +0200

    EpsodeListServlet

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/mediavote/episode/web/EpisodeListServlet.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2018 Dennis Schulmeister-Zimolong
+ * 
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.mediavote.episode.web;
+
+import dhbwka.wwi.vertsys.javaee.mediavote.common.ejb.UserBean;
+import dhbwka.wwi.vertsys.javaee.mediavote.common.jpa.User;
+import dhbwka.wwi.vertsys.javaee.mediavote.common.web.WebUtils;
+import dhbwka.wwi.vertsys.javaee.mediavote.episode.ejb.EpisodeBean;
+import dhbwka.wwi.vertsys.javaee.mediavote.episode.jpa.Episode;
+import dhbwka.wwi.vertsys.javaee.mediavote.score.ejb.ScoreBean;
+import dhbwka.wwi.vertsys.javaee.mediavote.tasks.jpa.Task;
+import java.io.IOException;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet für die tabellarische Auflisten der Aufgaben.
+ */
+@WebServlet(urlPatterns = {"/app/episode/list"})
+public class EpisodeListServlet extends HttpServlet {
+
+    @EJB
+    private ScoreBean scoreBean;
+    
+    @EJB
+    private EpisodeBean episodeBean;
+    
+    @EJB
+    private UserBean userBean;
+    
+
+   @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+               
+        
+        List<Episode> episodes = this.episodeBean.findAll();
+        request.setAttribute("episodes", episodes);
+              
+                
+        // Anfrage an dazugerhörige JSP weiterleiten
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/episode/list.jsp");
+        dispatcher.forward(request, response);
+        
+   
+        
+        
+
+
+
+    }
+
+}
