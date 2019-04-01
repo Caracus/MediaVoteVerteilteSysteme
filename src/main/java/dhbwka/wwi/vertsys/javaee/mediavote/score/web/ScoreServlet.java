c39a4dc4915285bda46ca460e74acfc9e05004d6
Johannes Hoppstädter <webmaster@hoppstaedter.net>
2019-04-01 21:10:30 +0200

    Bewertungen anlegen integriert

--- src/main/java/dhbwka/wwi/vertsys/javaee/mediavote/score/web/ScoreServlet.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/mediavote/score/web/ScoreServlet.java
@@ -11,11 +11,14 @@ package dhbwka.wwi.vertsys.javaee.mediavote.score.web;
 
 import dhbwka.wwi.vertsys.javaee.mediavote.common.ejb.UserBean;
 import dhbwka.wwi.vertsys.javaee.mediavote.common.jpa.User;
+import dhbwka.wwi.vertsys.javaee.mediavote.common.web.WebUtils;
 import dhbwka.wwi.vertsys.javaee.mediavote.episode.ejb.EpisodeBean;
 import dhbwka.wwi.vertsys.javaee.mediavote.episode.jpa.Episode;
 import dhbwka.wwi.vertsys.javaee.mediavote.score.ejb.ScoreBean;
 import dhbwka.wwi.vertsys.javaee.mediavote.score.jpa.Score;
+import dhbwka.wwi.vertsys.javaee.mediavote.tasks.jpa.TaskStatus;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
@@ -46,21 +49,23 @@ public class ScoreServlet extends HttpServlet {
             throws ServletException, IOException {
         
         User user = this.userBean.getCurrentUser();
-        
-        Episode episode = this.episodeBean.findById(51L);
-        
-        //Score von Nutzer vorhanden -> laden
-        
-        List<Score> scores = scoreBean.findByUserAndEpisode(user.getUsername(), episode.getId());
-        
-        //Score score = new Score(user, episode, 0);
-        
-        if (!scores.isEmpty()) {
+        List<Score> scores;
+        try {
+            Episode episode = getRequestedEpisode(request);
+            scores = scoreBean.findByUserAndEpisode(user.getUsername(), episode.getId());
+            request.setAttribute("episode", episode);
+            if (!scores.isEmpty()) {
             //Bewertung abgegeben, muss ein Element enhalten
             Score score = scores.get(0);
             request.setAttribute("score", score);
+            }
+        } catch(Exception e) {
+            
         }
-  
+        
+        //Werte setzen
+        request.setAttribute("user", user);
+        
         // Anfrage an dazugerhörige JSP weiterleiten
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/episode/score.jsp");
         dispatcher.forward(request, response);
@@ -69,11 +74,65 @@ public class ScoreServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-       
-
-      
         
+        User user = this.userBean.getCurrentUser();
+        /*
+        try {
+               
+        } catch(Exception e) {
+            
+        }*/
+
+        int rating = Integer.parseInt(request.getParameter("episode_score"));
+        Episode episode = getRequestedEpisode(request);    
+        
+        //Episode episode = new Episode(user, name, series, season, number, description);
+        
+        List<Score> scores = scoreBean.findByUserAndEpisode(user.getUsername(), episode.getId());
+        
+        if (!scores.isEmpty()) {
+           Score score = scores.get(0);
+           score.setRating(rating);
+           scoreBean.update(score); 
+        }
+        else {
+            Score score = new Score(user, episode, rating);
+            scoreBean.saveNew(score);
+        }
+        
+        
+
+        
+   
+   
+        // Keine Fehler: Startseite aufrufen
+        response.sendRedirect(WebUtils.appUrl(request, "/app/dashboard/"));   
+        
+    }
+    
+    private Episode getRequestedEpisode(HttpServletRequest request) {
+         // ID aus der URL herausschneiden
+        String episodeIdString = request.getPathInfo();
+
+        if (episodeIdString == null) {
+            episodeIdString = "/1";
+        }
+
+        episodeIdString = episodeIdString.substring(1);
+
+        if (episodeIdString.endsWith("/")) {
+            episodeIdString = episodeIdString.substring(0, episodeIdString.length() - 1);
+        }
+        
+        long episodeId = Long.parseLong(episodeIdString);
+        Episode episode;
+    
+        try {
+           episode = this.episodeBean.findById(episodeId); 
+        } catch(NumberFormatException e) {
+            throw e;
+        }       
+        return episode;
     }
 
 
